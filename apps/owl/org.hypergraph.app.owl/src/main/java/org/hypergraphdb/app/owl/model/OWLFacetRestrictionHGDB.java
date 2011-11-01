eae33632edebac113ff82c5781065082b5a8cb0a
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2011-11-01 16:15:38 +0000

    OwlFacet enum uses HG Enum type.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1474 43784a7f-e236-0410-9472-c78ae7dffccf

---
+++ apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/model/OWLFacetRestrictionHGDB.java
@@ -0,0 +1,126 @@
+package org.hypergraphdb.app.owl.model;
+
+import org.hypergraphdb.HGHandle;
+import org.hypergraphdb.HGLink;
+import org.hypergraphdb.app.owl.core.OWLObjectHGDB;
+import org.semanticweb.owlapi.model.OWLDataVisitor;
+import org.semanticweb.owlapi.model.OWLDataVisitorEx;
+import org.semanticweb.owlapi.model.OWLFacetRestriction;
+import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.owlapi.model.OWLObjectVisitor;
+import org.semanticweb.owlapi.model.OWLObjectVisitorEx;
+import org.semanticweb.owlapi.vocab.OWLFacet;
+
+
+/**
+ * OWLFacetRestrictionHGDB.
+ * @author Thomas Hilpold (CIAO/Miami-Dade County)
+ * @created Nov 1, 2011
+ */
+public class OWLFacetRestrictionHGDB extends OWLObjectHGDB implements HGLink, OWLFacetRestriction {
+
+	private OWLFacet facet; //index 0
+    //private OWLFacet facet;
+
+	private HGHandle facetValueHandle; //index 1
+    //private OWLLiteral facetValue;
+
+    public OWLFacetRestrictionHGDB(HGHandle...args) {    
+        //TODO assert arg[0] type OWLLiteral
+    	assert (args.length == 1);    	
+        facetValueHandle = args[0];    	
+    }
+
+    public OWLFacetRestrictionHGDB(OWLFacet facet, HGHandle facetValue) {
+    	//OWLFacet facet, OWLLiteral facetValue
+        this.facet = facet;
+        facetValueHandle = facetValue;
+    }
+
+
+    /**
+     * Gets the restricting facet for this facet restriction
+     */
+    public OWLFacet getFacet() {
+        return facet;
+    }
+
+    /**
+     * For Hypergraph bean inspection only.
+	 * @param facet the facet to set
+	 */
+	public void setFacet(OWLFacet facet) {
+		this.facet = facet;
+	}
+
+    /**
+     * Gets the corresponding facet value for this facet restriction
+     */
+    public OWLLiteral getFacetValue() {
+        return getHyperGraph().get(facetValueHandle);
+    }
+
+
+    public void accept(OWLObjectVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    public <O> O accept(OWLDataVisitorEx<O> visitor) {
+        return visitor.visit(this);
+    }
+
+    public void accept(OWLDataVisitor visitor) {
+        visitor.visit(this);
+    }
+
+    public <O> O accept(OWLObjectVisitorEx<O> visitor) {
+        return visitor.visit(this);
+    }
+
+    @Override
+	protected int compareObjectOfSameType(OWLObject object) {
+        OWLFacetRestriction other = (OWLFacetRestriction) object;
+        int diff = getFacet().compareTo(other.getFacet());
+        if (diff != 0) {
+            return diff;
+        }
+        return getFacetValue().compareTo(other.getFacetValue());
+    }
+
+	/* (non-Javadoc)
+	 * @see org.hypergraphdb.HGLink#getArity()
+	 */
+	@Override
+	public int getArity() {
+		return 1;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hypergraphdb.HGLink#getTargetAt(int)
+	 */
+	@Override
+	public HGHandle getTargetAt(int i) {
+		if (!(i >= 0 && i < getArity())) throw new IllegalArgumentException("Index has to be 0 and less than " + getArity());
+		return facetValueHandle;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hypergraphdb.HGLink#notifyTargetHandleUpdate(int, org.hypergraphdb.HGHandle)
+	 */
+	@Override
+	public void notifyTargetHandleUpdate(int i, HGHandle handle) {
+		if (!(i >= 0 && i < getArity())) throw new IllegalArgumentException("Index has to be 0 and less than " + getArity()); 
+		if (handle == null) throw new IllegalArgumentException("handle null"); 
+		facetValueHandle = handle;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hypergraphdb.HGLink#notifyTargetRemoved(int)
+	 */
+	@Override
+	public void notifyTargetRemoved(int i) {
+		if (!(i >= 0 && i < getArity())) throw new IllegalArgumentException("Index has to be 0 and less than " + getArity()); 
+		facetValueHandle = null;
+	}
+}
