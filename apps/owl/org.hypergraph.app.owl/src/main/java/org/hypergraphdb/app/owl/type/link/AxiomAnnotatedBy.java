ce4a309013e866d958ca64718810bb72bccea1f2
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2011-11-22 15:30:07 +0000

    cleanup.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1533 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/type/link/AxiomAnnotatedBy.java
+++ apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/type/link/AxiomAnnotatedBy.java
@@ -4,7 +4,8 @@ import org.hypergraphdb.HGHandle;
 import org.hypergraphdb.HGPlainLink;
 
 /**
- * AxiomAnnotatedBy connects Axioms to OWLAnnotationHGDB atoms. 
+ * AxiomAnnotatedBy connects Axioms [0] to OWLAnnotationHGDB atoms [1].
+ * One axiom is connected to 0..* OWLAnnotationHGDB atoms. 
  * 
  * @author Thomas Hilpold (GIC/Miami-Dade County)
  * @created Nov 18, 2011
