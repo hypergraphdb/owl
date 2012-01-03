cd0dea4e51c0548b72da3d80283db327ffa7ba58
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-01-03 22:46:54 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1703 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/exception/HGDBOntologyAlreadyExistsByOntologyIDException.java
+++ apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/exception/HGDBOntologyAlreadyExistsByOntologyIDException.java
@@ -1,6 +1,7 @@
 package org.hypergraphdb.app.owl.exception;
 
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyID;
 
 /**
  * HGDBOntologyAlreadyExistsByOntologyIDException.
@@ -8,5 +9,16 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
  * @created Jan 3, 2012
  */
 public class HGDBOntologyAlreadyExistsByOntologyIDException extends OWLOntologyCreationException {
+	
+	private OWLOntologyID ontologyID;
+	
+	public HGDBOntologyAlreadyExistsByOntologyIDException(OWLOntologyID ontologyID) {
+	    this.ontologyID = ontologyID;
+	}
+	
+	public OWLOntologyID getOntologyID() {
+	    return ontologyID;
+	}
+	
 
 }
