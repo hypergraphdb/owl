bd75a06b88dc1e3eeded6e40759260ccdb73c637
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-01-05 23:40:48 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1721 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/test/A000AllOntologyManagerTests.java
+++ apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/test/A000AllOntologyManagerTests.java
@@ -1,5 +1,6 @@
 package org.hypergraphdb.app.owl.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -11,15 +12,24 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-	  //T001Classes.class,
+	  //T001Classes.class
 	  //T002ClassExpressionTest.class,
 	  //T003PropertiesTest.class,
 	  //T004IndividualsTest.class,
 	  //T005LiteralsTest.class,
 		//T006DataRangeTest.class,
-	T007DataPropertyRestrictionTest.class
+	//T007DataPropertyRestrictionTest.class
+	  TG001GarbageCollectorTest.class
+	//P001RepositoryPerformanceTest.class
 })
 
 public class A000AllOntologyManagerTests {
 	//intentionally empty.
+	@Test
+	public void test() {
+
+	}
+	
 }
+
+
