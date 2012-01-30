d2dfd3d1732db96eeb9197b05c90ed16166db2da
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-01-30 23:00:33 +0000

    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@1888 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/versioning/change/VOWLChange.java
+++ apps/owl/org.hypergraph.app.owl/src/main/java/org/hypergraphdb/app/owl/versioning/change/VOWLChange.java
@@ -1,6 +1,7 @@
 package org.hypergraphdb.app.owl.versioning.change;
 
 import org.hypergraphdb.HGLink;
+import org.semanticweb.owlapi.model.AddAxiom;
 
 /**
  * Change.
@@ -10,7 +11,21 @@ import org.hypergraphdb.HGLink;
  */
 public abstract class VOWLChange implements HGLink {	
 	
+	public static boolean isAddChange(VOWLChange c) {
+		return c instanceof VAddAxiomChange || c instanceof VAddImportChange || c instanceof VAddOntologyAnnotationChange;
+	}
+
+	public static boolean isRemoveChange(VOWLChange c) {
+		return c instanceof VRemoveAxiomChange || c instanceof VRemoveAxiomChange || c instanceof VRemoveOntologyAnnotationChange;
+	}
+
+	public static boolean isModifyChange(VOWLChange c) {
+		return c instanceof VModifyOntologyIDChange;
+	}
+	
 	//public abstract OWLOntologyChange convertToOWLOntologyChange();
 	
 	//public abstract OWLOntologyChange convertToInverseOWLOntologyChange();
+	
+	
 }
