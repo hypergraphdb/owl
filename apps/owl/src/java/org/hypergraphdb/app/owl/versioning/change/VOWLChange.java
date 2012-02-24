f685fa7527a914b1a2587fcd6ec5c36ec7426356
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-02-24 21:04:14 +0000

    added/implemented VersioningObject / Visitor for rendering.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2065 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/src/java/org/hypergraphdb/app/owl/versioning/change/VOWLChange.java
+++ apps/owl/src/java/org/hypergraphdb/app/owl/versioning/change/VOWLChange.java
@@ -1,6 +1,8 @@
 package org.hypergraphdb.app.owl.versioning.change;
 
 import org.hypergraphdb.HGLink;
+import org.hypergraphdb.app.owl.versioning.VersioningObject;
+import org.hypergraphdb.app.owl.versioning.VersioningObjectVisitor;
 import org.semanticweb.owlapi.model.AddAxiom;
 
 /**
@@ -9,7 +11,7 @@ import org.semanticweb.owlapi.model.AddAxiom;
  * @author Thomas Hilpold (CIAO/Miami-Dade County)
  * @created Jan 13, 2012
  */
-public abstract class VOWLChange implements HGLink {	
+public abstract class VOWLChange implements HGLink, VersioningObject {	
 	
 	public static boolean isAddChange(VOWLChange c) {
 		return c instanceof VAddAxiomChange || c instanceof VAddImportChange || c instanceof VAddOntologyAnnotationChange;
@@ -21,6 +23,14 @@ public abstract class VOWLChange implements HGLink {
 
 	public static boolean isModifyChange(VOWLChange c) {
 		return c instanceof VModifyOntologyIDChange;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hypergraphdb.app.owl.versioning.VersioningObject#accept(org.hypergraphdb.app.owl.versioning.VersioningObjectVisitor)
+	 */
+	@Override
+	public void accept(VersioningObjectVisitor visitor) {
+		visitor.visit(this);
 	}
 	
 	//public abstract OWLOntologyChange convertToOWLOntologyChange();
