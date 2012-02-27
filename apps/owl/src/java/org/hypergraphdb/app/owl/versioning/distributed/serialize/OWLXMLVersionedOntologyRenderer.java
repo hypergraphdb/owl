049b0605a324293452bb37d9be260f458d70ce94
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-02-27 23:46:38 +0000

    first renderer version finished.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2073 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/src/java/org/hypergraphdb/app/owl/versioning/distributed/serialize/OWLXMLVersionedOntologyRenderer.java
+++ apps/owl/src/java/org/hypergraphdb/app/owl/versioning/distributed/serialize/OWLXMLVersionedOntologyRenderer.java
@@ -1,20 +1,80 @@
 package org.hypergraphdb.app.owl.versioning.distributed.serialize;
 
-import org.coode.owlapi.owlxml.renderer.OWLXMLRenderer;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.hypergraphdb.app.owl.HGDBOntologyManagerImpl;
+import org.hypergraphdb.app.owl.HGDBOntologyRepository;
+import org.hypergraphdb.app.owl.versioning.VHGDBOntologyRepository;
+import org.hypergraphdb.app.owl.versioning.VersionedOntology;
+import org.semanticweb.owlapi.io.AbstractOWLRenderer;
+import org.semanticweb.owlapi.io.OWLRendererException;
+import org.semanticweb.owlapi.io.OWLRendererIOException;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.vocab.Namespaces;
 
 /**
  * OWLXMLVersionedOntologyRenderer.
  * @author Thomas Hilpold (CIAO/Miami-Dade County)
  * @created Feb 24, 2012
  */
-public class OWLXMLVersionedOntologyRenderer extends OWLXMLRenderer {
+public class OWLXMLVersionedOntologyRenderer extends AbstractOWLRenderer {
 
 	/**
 	 * @param owlOntologyManager
 	 */
 	public OWLXMLVersionedOntologyRenderer(OWLOntologyManager owlOntologyManager) {
 		super(owlOntologyManager);
-		// TODO Auto-generated constructor stub
 	}
+
+	/* (non-Javadoc)
+	 * @see org.semanticweb.owlapi.io.AbstractOWLRenderer#render(org.semanticweb.owlapi.model.OWLOntology, java.io.Writer)
+	 */
+	@Override
+	public void render(OWLOntology ontology, Writer writer) throws OWLRendererException {
+		OWLOntologyManager m = getOWLOntologyManager();
+		if (m instanceof HGDBOntologyManagerImpl) {
+			HGDBOntologyRepository ho = ((HGDBOntologyManagerImpl) m).getOntologyRepository();
+			if (ho instanceof VHGDBOntologyRepository) {
+				VHGDBOntologyRepository vrep = (VHGDBOntologyRepository)ho;
+				VersionedOntology vo = vrep.getVersionControlledOntology(ontology);
+				if (vo != null) {
+					render(vo, writer);
+				} else {
+					new OWLRendererException("The given ontology is not versioned." + ontology);
+				}
+			} else {
+				throw new OWLRendererException("Need a versioned VHGDBOntologyRepository in the manager.");
+			}
+		} else {
+			throw new OWLRendererException("Need a HGDBOntologyManagerImpl as manager.");
+		}
+	}
+
+	public void render(VersionedOntology vonto, Writer writer) throws OWLRendererException {
+    	render(vonto, writer, new VersionedOntologyRenderConfiguration());
+    }
+
+    public void render(VersionedOntology vonto, Writer writer, VersionedOntologyRenderConfiguration configuration) throws OWLRendererException {
+        try {
+        	OWLXMLVersionedOntologyWriter vw = new OWLXMLVersionedOntologyWriter(writer, vonto);
+            
+            vw.startDocument(vonto);
+
+            vw.writePrefix("rdf:", Namespaces.RDF.toString());
+            vw.writePrefix("rdfs:", Namespaces.RDFS.toString());
+            vw.writePrefix("xsd:", Namespaces.XSD.toString());
+            vw.writePrefix("owl:", Namespaces.OWL.toString());
+
+            OWLXMLVersioningObjectRenderer vren = new OWLXMLVersioningObjectRenderer(vw, configuration);
+            configuration.accept(vren);
+            vonto.accept(vren);
+            vw.endDocument();
+            writer.flush();
+        }
+        catch (IOException e) {
+            throw new OWLRendererIOException(e);
+        }
+    }
 }
