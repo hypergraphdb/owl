e10d216e6061c3a73027b0185928e915d6019c28
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-03-12 22:49:27 +0000

    moving test to other class.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2141 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/src/java/org/hypergraphdb/app/owl/versioning/distributed/VDHGDBOntologyRepository.java
+++ apps/owl/src/java/org/hypergraphdb/app/owl/versioning/distributed/VDHGDBOntologyRepository.java
@@ -1,8 +1,5 @@
 package org.hypergraphdb.app.owl.versioning.distributed;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -16,38 +13,15 @@ import java.util.concurrent.TimeoutException;
 
 import org.hypergraphdb.HGHandle;
 import org.hypergraphdb.HGQuery.hg;
-import org.hypergraphdb.app.owl.HGDBOWLManager;
-import org.hypergraphdb.app.owl.HGDBOntology;
-import org.hypergraphdb.app.owl.HGDBOntologyManagerImpl;
 import org.hypergraphdb.app.owl.HGDBOntologyRepository;
-import org.hypergraphdb.app.owl.usage.ImportOntologies;
 import org.hypergraphdb.app.owl.versioning.VHGDBOntologyRepository;
 import org.hypergraphdb.app.owl.versioning.VersionedOntology;
 import org.hypergraphdb.app.owl.versioning.distributed.activity.PushVersionedOntology;
-import org.hypergraphdb.app.owl.versioning.distributed.serialize.VOWLXMLParser;
-import org.hypergraphdb.app.owl.versioning.distributed.serialize.VOWLXMLVersionedOntologyRenderer;
-import org.hypergraphdb.app.owl.versioning.distributed.serialize.VOWLRenderConfiguration;
-import org.hypergraphdb.app.owl.versioning.distributed.serialize.VOWLXMLDocument;
 import org.hypergraphdb.peer.HGPeerIdentity;
 import org.hypergraphdb.peer.HyperGraphPeer;
 import org.hypergraphdb.peer.PeerPresenceListener;
 import org.hypergraphdb.peer.workflow.Activity;
 import org.jivesoftware.smack.XMPPConnection;
-import org.semanticweb.owlapi.io.FileDocumentSource;
-import org.semanticweb.owlapi.io.OWLOntologyDocumentSource;
-import org.semanticweb.owlapi.io.OWLParserException;
-import org.semanticweb.owlapi.io.OWLRendererException;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyChangeException;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyID;
-import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
-import org.semanticweb.owlapi.model.RemoveAxiom;
-import org.semanticweb.owlapi.model.UnloadableImportException;
-
-import uk.ac.manchester.cs.owl.owlapi.OWLOntologyImpl;
 
 /**
  * VDHGDBOntologyRepository.
@@ -242,114 +216,5 @@ public class VDHGDBOntologyRepository extends VHGDBOntologyRepository {
 	
 	public enum RemoteRepositoryActionResult {SUCCESS, DENIED_LOCAL_OUT_OF_DATE, DENIED_WERE_EQUAL, DENIED_REMOTE_OUT_OF_DATE}; 
 	
-	public static void main(String[] argv) {
-		VDHGDBOntologyRepository.PEER_USERNAME = argv[0];
-		VDHGDBOntologyRepository.PEER_PASSWORD = argv[1];
-		File dir = new File ("C:\\temp\\hypergraph-" + VDHGDBOntologyRepository.PEER_USERNAME);
-		if (!dir.exists()) dir.mkdir();
-		VDHGDBOntologyRepository.setHypergraphDBLocation(dir.getAbsolutePath());
-		VDHGDBOntologyRepository dr = VDHGDBOntologyRepository.getInstance();
-		if (argv[0].contains("1")) {
-			ensureOneVersionedOntology(dr);
-		}
-//		dr.startNetworking();
-//		dr.printPeerInfo();
-//		try {
-//			while (true) {
-//				Thread.sleep((long) 10000);
-//				dr.printPeerInfo();
-//				dr.push(null, null);
-//			}
-//			
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		} finally {
-//			dr.stopNetworking();
-//		}
-	}
 
-	/**
-	 * @param dr
-	 */
-	private static void ensureOneVersionedOntology(VDHGDBOntologyRepository dr) {
-		List <File> renderedFiles = new ArrayList<File>();
-		HGDBOntologyManagerImpl manager = HGDBOWLManager.createOWLOntologyManager();
-		//
-		// IMPORT AND RENDER
-		//
-//		try {
-//			VDHGDBOntologyRepository repo = (VDHGDBOntologyRepository)manager.getOntologyRepository();
-//			//repo.dropHypergraph();
-//			repo.deleteAllOntologies();
-//			//System.out.println("Running GC");
-//			//CANNOT RUN GC nullHANDLE problem !!! repo.runGarbageCollector();
-//			File f = new File("C:\\_CiRM\\testontos\\County.owl");
-//			IRI targetIRI = ImportOntologies.importOntology(f, manager);
-//			//File f2 = new File("C:\\_CiRM\\testontos\\1 csr.owl");
-//			//IRI targetIRI = ImportOntologies.importOntology(f2, manager);
-//			HGDBOntology o = (HGDBOntology)manager.loadOntologyFromOntologyDocument(targetIRI);
-//			VersionedOntology vo = repo.addVersionControl(o, "distributedTestUser");
-//			// MANIPULATE REMOVE CHANGED
-//			Object[] axioms = o.getAxioms().toArray();
-//			//remove all axioms 10.
-//			for (int i = 0; i < axioms.length / 10; i ++) {
-//				int j = i;
-//				for (;j < i + axioms.length / 100; j++) {
-//					if (j < axioms.length) {
-//						manager.applyChange(new RemoveAxiom(o, (OWLAxiom)axioms[j]));
-//					}
-//				}
-//				i = j;
-//				vo.commit("SameUser", " commit no " + i);
-//			}
-//			// RENDER VERSIONED ONTOLOGY, includes data
-//			for (int i = 0; i < vo.getArity(); i ++) {
-//				VOWLRenderConfiguration c = new VOWLRenderConfiguration();
-//				c.setLastRevisionIndex(i);
-//				VOWLXMLVersionedOntologyRenderer r = new VOWLXMLVersionedOntologyRenderer(manager);
-//				File fx = new File("C:\\_CiRM\\testontos\\CountyVersioned-Rev-"+ i + ".vowlxml");
-//				renderedFiles.add(fx);
-//				//File fx = new File("C:\\_CiRM\\testontos\\1 csr-Rev-"+ i + ".vowlxml");
-//				FileWriter fwriter = new FileWriter(fx);
-//				//	Full export
-//				r.render(vo, fwriter, c);
-//			}
-//		} catch (OWLOntologyCreationException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (OWLRendererException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		//
-		// PARSE
-		//
-		//for (File f : renderedFiles) {
-		File f = new File("C:\\_CiRM\\testontos\\CountyVersioned-Rev-"+ 10 + ".vowlxml");
-			System.out.println("Parsing: " + f + " length: " + (f.length() / 1024) + " kB");
-			OWLOntologyDocumentSource source = new FileDocumentSource(f);
-			VOWLXMLParser parser = new VOWLXMLParser();
-			OWLOntology onto = new OWLOntologyImpl(manager, new OWLOntologyID());
-			// must have onto for manager in super class
-			VOWLXMLDocument versionedOntologyRoot = new VOWLXMLDocument(onto);
-			try {
-				parser.parse(source, versionedOntologyRoot, new OWLOntologyLoaderConfiguration());
-				System.out.println("PARSING FINISHED.");
-			} catch (OWLOntologyChangeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnloadableImportException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (OWLParserException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		//}
-	}
 }
