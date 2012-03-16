d1c47f273bc95a060e862b5204deb3d9a8925ded
thomas.hilpold@gmail.com <thomas.hilpold@gmail.com@43784a7f-e236-0410-9472-c78ae7dffccf>
2012-03-16 23:09:20 +0000

    removed: Handleholder interface added: equals and hashcode implementation.
    git-svn-id: https://hypergraphdb.googlecode.com/svn/trunk@2170 43784a7f-e236-0410-9472-c78ae7dffccf

--- apps/owl/src/java/org/hypergraphdb/app/owl/versioning/Revision.java
+++ apps/owl/src/java/org/hypergraphdb/app/owl/versioning/Revision.java
@@ -2,16 +2,12 @@ package org.hypergraphdb.app.owl.versioning;
 
 import java.util.Date;
 
-import org.hypergraphdb.HGHandle;
-import org.hypergraphdb.HGHandleHolder;
-
-
 /**
  * Revision represents the first ontology or a revised version of the ontology.
  * @author Thomas Hilpold (CIAO/Miami-Dade County)
  * @created Jan 18, 2012
  */
-public class Revision extends RevisionID implements HGHandleHolder, VersioningObject {
+public class Revision extends RevisionID implements /* HGHandleHolder , */ VersioningObject {
 	
 	public static final long TIMESTAMP_UNDEFINED = Long.MIN_VALUE; 
 	public static final String USER_ANONYMOUS = null; 
@@ -20,7 +16,7 @@ public class Revision extends RevisionID implements HGHandleHolder, VersioningOb
 	private String user; 
 	private String revisionComment;
 //	boolean hasRevisionData;
-	private HGHandle handle;
+	//private HGHandle handle;
 
 	public Revision() {
 		//do nothing
@@ -82,22 +78,22 @@ public class Revision extends RevisionID implements HGHandleHolder, VersioningOb
 //		this.hasRevisionData = hasRevisionData;
 //	}
 
-	/* (non-Javadoc)
-	 * @see org.hypergraphdb.HGHandleHolder#getAtomHandle()
-	 */
-	@Override
-	public HGHandle getAtomHandle() {
-		return handle;
-	}
+//	/* (non-Javadoc)
+//	 * @see org.hypergraphdb.HGHandleHolder#getAtomHandle()
+//	 */
+//	@Override
+//	public HGHandle getAtomHandle() {
+//		return handle;
+//	}
 
-	/* (non-Javadoc)
-	 * @see org.hypergraphdb.HGHandleHolder#setAtomHandle(org.hypergraphdb.HGHandle)
-	 */
-	@Override
-	public void setAtomHandle(HGHandle handle) {
-		this.handle = handle;
-		
-	}
+//	/* (non-Javadoc)
+//	 * @see org.hypergraphdb.HGHandleHolder#setAtomHandle(org.hypergraphdb.HGHandle)
+//	 */
+//	@Override
+//	public void setAtomHandle(HGHandle handle) {
+//		this.handle = handle;
+//		
+//	}
 	
 	/* (non-Javadoc)
 	 * @see org.hypergraphdb.app.owl.versioning.VersioningObject#accept(org.hypergraphdb.app.owl.versioning.VOWLObjectVisitor)
@@ -107,4 +103,37 @@ public class Revision extends RevisionID implements HGHandleHolder, VersioningOb
 		visitor.visit(this);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return super.hashCode() + 7 * ((user != null? user.hashCode(): 0) 
+				+ (timeStamp != null? timeStamp.hashCode(): 0) 
+				+ (revisionComment != null? revisionComment.hashCode(): 0)); 
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object anObject) {
+		if (this == anObject) {
+		    return true;
+		}
+		if (anObject instanceof Revision) {
+			Revision other = (Revision) anObject;
+			return super.equals(other) 
+				&& equalsInclNull(user, other.user)
+				&& equalsInclNull(timeStamp, other.timeStamp)
+				&& equalsInclNull(revisionComment, other.revisionComment);
+		} else {
+			return false;
+		}
+	}
+
+	protected boolean equalsInclNull(Object thiso, Object other) {
+		return (thiso == null && other == null)
+			|| (thiso != null && thiso.equals(other));
+	}	
 }
